3eaa2e0e825a6593ad71c249cf2e7e6a244fb6e9
ixotum <ixotum@gmail.com>
2018-02-28 20:29:01 +0200

    [I] injector added

--- src/main/java/com/ksilit/Main.java
+++ src/main/java/com/ksilit/Main.java
@@ -1,12 +1,25 @@
 package com.ksilit;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.ksilit.core.InjectorModule;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
+public class Main extends Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         LOGGER.info("ksilit starting...");
+
+        launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        final InjectorModule injectorModule = new InjectorModule();
+        final Injector injector = Guice.createInjector(injectorModule);
     }
 }
---
+++ src/main/java/com/ksilit/core/InjectorModule.java
@@ -0,0 +1,10 @@
+package com.ksilit.core;
+
+import com.google.inject.AbstractModule;
+
+public class InjectorModule extends AbstractModule {
+    @Override
+    protected void configure() {
+
+    }
+}
